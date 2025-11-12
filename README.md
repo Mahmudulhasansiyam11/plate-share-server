6c65519c1aedc35184fe16625aae28a76d1ff5d2
Mahmudulhasansiyam11 <mahmudulsiyam766@gmail.com>
2025-11-12 04:46:54 -0800

    add readme.md file

---
+++ README.md
@@ -0,0 +1,5 @@
+### Assignment 10
+
+### Website Name: PlateShare
+
+### Server site URL: plate-share-api-server.vercel.app
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("plate_db");
     const foodsCollection = db.collection("foods");
@@ -174,7 +174,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
